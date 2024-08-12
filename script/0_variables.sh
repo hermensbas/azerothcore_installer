51dc9a79586e23e049a062270a1479c4d0190bdd
bash <31279994+hermensbas@users.noreply.github.com>
2024-08-12 23:33:15 +0200

    Update 0_variables.sh

--- script/0_variables.sh
+++ script/0_variables.sh
@@ -3,16 +3,16 @@
 ##########################################################################################
 # INTERNAL CONFIG
 ##########################################################################################
-ACORE_SCRIPT="${ROOT_DIR}/_server/azerothcore/acore.sh"
-#AUTHSERVER="${ROOT_DIR}/_server/azerothcore/acore.sh run-authserver"
-#WORLDSERVER="${ROOT_DIR}/_server/azerothcore/acore.sh run-worldserver"
+SERVER_ROOT="${ROOT_DIR}/_server/azerothcore"
+DATAPATH="${SERVER_ROOT}/env/dist/bin"
+
 AUTHSERVER_SESSION="auth-session"
 WORLDSERVER_SESSION="world-session"
+
 MYSQL_ROOT_PASSWORD="rootpassword"
 MYSQL_USER="acore"
 MYSQL_PASSWORD="acore"
 MYSQL_DATABASE="acore_auth"
-DATAPATH="${ROOT_DIR}/_server/azerothcore/env/dist/bin"
 
 ##########################################################################################
 # add chmod rights
@@ -36,19 +36,22 @@ fi
 # add variables to ~/.bash_wow
 ##########################################################################################
 echo "export ROOT_DIR=${ROOT_DIR}" > ~/.bash_wow
-echo "export REALM_IP=${REALM_IP}" >> ~/.bash_wow
-echo "export REALM_NAME=${REALM_NAME}" >> ~/.bash_wow
-echo "export AH_BOT_MIN_ITEMS=${AH_BOT_MIN_ITEMS}" >> ~/.bash_wow
-echo "export AH_BOT_MAX_ITEMS=${AH_BOT_MAX_ITEMS}" >> ~/.bash_wow
+echo "export SERVER_ROOT=${SERVER_ROOT}" >> ~/.bash_wow
+echo "export DATAPATH=${DATAPATH}" >> ~/.bash_wow
 
-echo "export ACORE_SCRIPT=${ACORE_SCRIPT}" >> ~/.bash_wow
 echo "export AUTHSERVER_SESSION=${AUTHSERVER_SESSION}" >> ~/.bash_wow
 echo "export WORLDSERVER_SESSION=${WORLDSERVER_SESSION}" >> ~/.bash_wow
+
 echo "export MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}" >> ~/.bash_wow
 echo "export MYSQL_USER=${MYSQL_USER}" >> ~/.bash_wow
 echo "export MYSQL_PASSWORD=${MYSQL_PASSWORD}" >> ~/.bash_wow
 echo "export MYSQL_DATABASE=${MYSQL_DATABASE}" >> ~/.bash_wow
-echo "export DATAPATH=${DATAPATH}" >> ~/.bash_wow
+
+echo "export REALM_IP=${REALM_IP}" >> ~/.bash_wow
+echo "export REALM_NAME=${REALM_NAME}" >> ~/.bash_wow
+
+echo "export AH_BOT_MIN_ITEMS=${AH_BOT_MIN_ITEMS}" >> ~/.bash_wow
+echo "export AH_BOT_MAX_ITEMS=${AH_BOT_MAX_ITEMS}" >> ~/.bash_wow
 
 ##########################################################################################
 #  add alias to ~/.bash_wow
@@ -61,5 +64,5 @@ echo "alias compile='${ROOT_DIR}/script/4_server-compile.sh'" >> ~/.bash_wow
 echo "alias config='${ROOT_DIR}/script/5_server-configure.sh'" >> ~/.bash_wow
 echo "alias start='${ROOT_DIR}/script/6_server-start.sh'" >> ~/.bash_wow
 echo "alias stop='tmux kill-server'" >> ~/.bash_wow
-echo "alias wow='cd ${ROOT_DIR}/_server/azerothcore;tmux attach -t ${WORLDSERVER_SESSION}'" >> ~/.bash_wow
-echo "alias auth='cd ${ROOT_DIR}/_server/azerothcore;tmux attach -t ${AUTHSERVER_SESSION}'" >> ~/.bash_wow
+echo "alias wow='cd ${SERVER_ROOT};tmux attach -t ${WORLDSERVER_SESSION}'" >> ~/.bash_wow
+echo "alias auth='cd ${SERVER_ROOT};tmux attach -t ${AUTHSERVER_SESSION}'" >> ~/.bash_wow
