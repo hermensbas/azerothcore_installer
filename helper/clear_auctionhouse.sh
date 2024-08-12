ff67cbed927b86c92abfcff2abeba19c707d9939
bash <31279994+hermensbas@users.noreply.github.com>
2024-08-12 21:03:04 +0200

    Update clear_auctionhouse.sh

--- helper/clear_auctionhouse.sh
+++ helper/clear_auctionhouse.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-ROOT_DIR="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
-source ${ROOT_DIR}/../variables.sh
 
-sudo mysql -u root --database="acore_characters" < ${ROOT_DIR}/../sql/clear_auctionhouse.sql
+sudo mysql -u root --database="acore_characters" < ${ROOT_DIR}/sql/clear_auctionhouse.sql
 
 echo "Auction house items deleted..."
