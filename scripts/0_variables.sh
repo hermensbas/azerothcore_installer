34bae5ebe160020b6e9380213732029e931e54b3
bash <31279994+hermensbas@users.noreply.github.com>
2024-08-12 21:42:42 +0200

    Update 0_variables.sh

--- scripts/0_variables.sh
+++ scripts/0_variables.sh
@@ -1,28 +1,4 @@
 #!/bin/bash
-ROOT_DIR="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
-
-##########################################################################################
-# USER CONFIG
-##########################################################################################
-REALM_IP="192.168.178.185"
-REALM_NAME="azerothcore.org"
-AH_BOT_MIN_ITEMS="25000"
-AH_BOT_MAX_ITEMS="30000"
-
-
-##########################################################################################
-# INTERNAL CONFIGS
-# https://github.com/azerothcore/azerothcore-wotlk/blob/master/conf/dist
-##########################################################################################
-AUTHSERVER="${ROOT_DIR}/_server/azerothcore/acore.sh run-authserver"
-WORLDSERVER="${ROOT_DIR}/_server/azerothcore/acore.sh run-worldserver"
-AUTHSERVER_SESSION="auth-session"
-WORLDSERVER_SESSION="world-session"
-MYSQL_ROOT_PASSWORD="rootpassword"
-MYSQL_USER="acore"
-MYSQL_PASSWORD="acore"
-MYSQL_DATABASE="acore_auth"
-DATAPATH="${ROOT_DIR}/_server/azerothcore/env/dist/bin"
 
 #----------------------------------------------------------------------------------------
 # add chmod rights
