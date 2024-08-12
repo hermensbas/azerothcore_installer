64363341b5dd4567f4e57358f1463892fb37ede4
bash <31279994+hermensbas@users.noreply.github.com>
2024-08-12 22:18:06 +0200

    Create menu-print.sh

---
+++ script/menu-print.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "### menu    -  Shows menu"
+echo "### install -  Download the source source-code"
+echo "### update  -  Updates source-code to latest version"
+echo "### build   -  Builds the server based on source-code"
+echo "### compile -  Compiles the code based on the build server"
+echo "### config  -  Applies the server configration"
+echo "### start   -  starts auth and world in tmux sessions server"
+echo "### stop    -  stops all sessions"
+echo "### wow     -  logon world-server (tmux) session"
+echo "### auth    -  logon auth-server (tmux) session"
