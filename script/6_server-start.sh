a7f6a0fac0661112527cb61bc8bf3e8200dea90e
bash <31279994+hermensbas@users.noreply.github.com>
2024-08-12 22:16:13 +0200

    Update 6_server-start.sh

--- script/6_server-start.sh
+++ script/6_server-start.sh
@@ -57,6 +57,7 @@ fi
 echo " "
 echo "###########################################################################################"
 echo "## After the above you can use the following commands:"
+echo "### menu    -  Shows menu"
 echo "### install -  Download the source source-code"
 echo "### update  -  Updates source-code to latest version"
 echo "### build   -  Builds the server based on source-code"
