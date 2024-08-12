831779a93a8b31d9ee25fa4be6402b02ddc6f23d
bash <31279994+hermensbas@users.noreply.github.com>
2024-08-12 21:25:08 +0200

    Update 0_variables.sh

--- 0_variables.sh
+++ 0_variables.sh
@@ -30,7 +30,7 @@ sudo chmod +x ${ROOT_DIR}/*.sh
 sudo chmod +x ${ROOT_DIR}/helper/*.sh
 
 #----------------------------------------------------------------------------------------
-# Configure bash with variables above and alias
+# Add ~/.bash_wow to ~/.bashrc if not exist
 #----------------------------------------------------------------------------------------
 if grep -R "bash_wow" ~/.bashrc
 then
@@ -41,13 +41,15 @@ else
     echo "  . ~/.bash_wow" >> ~/.bashrc
     echo "fi" >> ~/.bashrc
 fi
-    
+
+#----------------------------------------------------------------------------------------
+# Add variables to ~/.bash_wow
+#----------------------------------------------------------------------------------------
 echo "export ROOT_DIR=${ROOT_DIR}" > ~/.bash_wow
 echo "export REALM_IP=${REALM_IP}" >> ~/.bash_wow
 echo "export REALM_NAME=${REALM_NAME}" >> ~/.bash_wow
 echo "export AH_BOT_MIN_ITEMS=${AH_BOT_MIN_ITEMS}" >> ~/.bash_wow
 echo "export AH_BOT_MAX_ITEMS=${AH_BOT_MAX_ITEMS}" >> ~/.bash_wow
-
 echo "export AUTHSERVER=${AUTHSERVER}" >> ~/.bash_wow
 echo "export WORLDSERVER=${WORLDSERVER}" >> ~/.bash_wow
 echo "export AUTHSERVER_SESSION=${AUTHSERVER_SESSION}" >> ~/.bash_wow
@@ -58,6 +60,9 @@ echo "export MYSQL_PASSWORD=${MYSQL_PASSWORD}" >> ~/.bash_wow
 echo "export MYSQL_DATABASE=${MYSQL_DATABASE}" >> ~/.bash_wow
 echo "export DATAPATH=${DATAPATH}" >> ~/.bash_wow
 
+#----------------------------------------------------------------------------------------
+# Add alias to ~/.bash_wow
+#----------------------------------------------------------------------------------------
 echo "alias update='${ROOT_DIR}/2_source-update.sh'" >> ~/.bash_wow
 echo "alias build='${ROOT_DIR}/3_server-build.sh'" >> ~/.bash_wow
 echo "alias compile='${ROOT_DIR}/4_server-compile.sh'" >> ~/.bash_wow
@@ -67,6 +72,9 @@ echo "alias stop='tmux kill-server'" >> ~/.bash_wow
 echo "alias wow='cd ${ROOT_DIR}/_server/azerothcore;tmux attach -t ${WORLDSERVER_SESSION}'" >> ~/.bash_wow
 echo "alias auth='cd ${ROOT_DIR}/_server/azerothcore;tmux attach -t ${AUTHSERVER_SESSION}'" >> ~/.bash_wow
 
+#----------------------------------------------------------------------------------------
+# Print alias
+#----------------------------------------------------------------------------------------
 echo " "
 echo "###########################################################################################"
 echo "## After the first install or changes either reboot or TYPE 'source ~/.bashrc' !!!!!!!!"
