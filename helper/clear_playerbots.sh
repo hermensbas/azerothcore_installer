734addb1d0cb021ef2acd6528c84a21203569078
bash <31279994+hermensbas@users.noreply.github.com>
2024-08-12 21:03:15 +0200

    Update clear_playerbots.sh

--- helper/clear_playerbots.sh
+++ helper/clear_playerbots.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-ROOT_DIR="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"/
-source ${ROOT_DIR}/../variables.sh
 
-sudo mysql -u root --database="acore_characters" < ${ROOT_DIR}/../sql/clear_playerbots.sql
+sudo mysql -u root --database="acore_characters" < ${ROOT_DIR}/sql/clear_playerbots.sql
 
 echo "Playerbots accounts and characters deleted..."
