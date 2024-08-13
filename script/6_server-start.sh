1584d75e5fdc3497e127c06f108ca9f3c916af86
bash <31279994+hermensbas@users.noreply.github.com>
2024-08-13 22:03:46 +0200

    Update 6_server-start.sh

--- script/6_server-start.sh
+++ script/6_server-start.sh
@@ -1,28 +1,6 @@
 #!/bin/bash
 
 ##########################################################################################
-# Apply server realm configuration
-##########################################################################################
-source ${ROOT_DIR}/script/update_realm_ip.sh ${REALM_IP}
-source ${ROOT_DIR}/script/update_realm_name.sh ${REALM_NAME}
-echo " "
-
-
-##########################################################################################
-# Apply ah-bot custom configuration
-##########################################################################################
-source ${ROOT_DIR}/script/update_ahbot_config.sh ${AH_BOT_MIN_ITEMS} ${AH_BOT_MAX_ITEMS}
-echo " "
-
-
-##########################################################################################
-# Clear playerbots account and characters (enforces performance and bot levels balance)
-##########################################################################################
-source ${ROOT_DIR}/script/clear_playerbots.sh
-echo " "
-
-
-##########################################################################################
 # handle the tmux sessions
 ##########################################################################################
 if tmux new-session -d -s $AUTHSERVER_SESSION; then
