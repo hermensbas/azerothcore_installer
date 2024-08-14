f7d66e688eb7b8fa99aa3ccc1778bb0d7d81aa5a
bash <31279994+hermensbas@users.noreply.github.com>
2024-08-14 20:03:19 +0200

    Update 0_variables.sh

--- script/0_variables.sh
+++ script/0_variables.sh
@@ -5,11 +5,12 @@
 ##########################################################################################
 AC_WOTLK_REPO="https://github.com/liyunfan1223/azerothcore-wotlk.git"
 AC_WOTLK_REPO_BRANCH="Playerbot"
+
 AC_MODE_PLAYER_BOTS_REPO="https://github.com/liyunfan1223/mod-playerbots.git"
 AC_MODE_PLAYER_BOTS_REPO_BRANCH="master"
+
 AC_MODE_AHBOT_REPO="https://github.com/azerothcore/mod-ah-bot.git"
 AC_MODE_AHBOT_REPO_BRANCH="master"
-
 
 SERVER_ROOT="${ROOT_DIR}/_server/azerothcore"
 DATAPATH="${SERVER_ROOT}/env/dist/bin"
