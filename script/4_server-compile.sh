c71f3ac49cd506ed84ac97dc5400663f647016bc
bashermens <31279994+hermensbas@users.noreply.github.com>
2026-01-10 21:15:24 +0100

    Update 4_server-compile.sh

--- script/4_server-compile.sh
+++ script/4_server-compile.sh
@@ -34,7 +34,6 @@ then
     # in that case 'compile build' is enough and much much faster
     # https://www.azerothcore.org/wiki/linux-core-installation
     ##########################################################################################
-    export CTYPE=RelWithDebInfo
     sudo ${ROOT_DIR}/_server/azerothcore/acore.sh compiler clean
     sudo -E ${ROOT_DIR}/_server/azerothcore/acore.sh compiler configure
     sudo ${ROOT_DIR}/_server/azerothcore/acore.sh compiler compile
