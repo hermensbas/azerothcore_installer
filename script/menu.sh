28c1c9e982c9608f66f9605b31f6249ffa7920bf
bashermens <31279994+hermensbas@users.noreply.github.com>
2026-01-10 21:12:45 +0100

    Update menu.sh

--- script/menu.sh
+++ script/menu.sh
@@ -5,8 +5,8 @@ echo "### menu    -  Shows menu"
 echo "### install -  Download the source source-code"
 echo "### update  -  Updates source-code to latest version"
 echo "### build   -  Builds the server based on source-code"
-echo "### compile -  Compiles the code based on the build server"
-echo "### compile debug -  Compiles the code based on the build server with RelWithDebInfo"
+echo "### compile -  Compiles code with Release for hosting"
+echo "### compile debug -  Compiles code with RelWithDebInfo for debugging"
 echo "### config  -  Applies the server configration"
 echo "### start   -  starts auth and world in tmux sessions server"
 echo "### debug   -  starts auth and world debug mode (GDB, timestamped crash logs)"
@@ -14,6 +14,3 @@ echo "### stop    -  stops all sessions"
 echo "### wow     -  logon world-server (tmux) session"
 echo "### auth    -  logon auth-server (tmux) session"
 echo "###########################################################################################"
-
-
-RelWithDebInfo
