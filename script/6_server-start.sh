1ecfa87583f148c9bcbee4a0365f2264f3c743e1
bashermens <31279994+hermensbas@users.noreply.github.com>
2026-01-30 09:51:01 +0100

    Update 6_server-start.sh

--- script/6_server-start.sh
+++ script/6_server-start.sh
@@ -80,7 +80,7 @@ if [[ $DEBUG_MODE -eq 1 ]]; then
     echo "When crash type: 'bt full' and then 'thread apply all bt'"
     WORLD_CMD="cd $ROOT/_server/azerothcore/env/dist/bin && \
         gdb \
-          -ex \"set inferior-tty /dev/tty\" \
+          -ex \"set inferior-tty /proc/$$/fd/1\" \
           -ex \"set logging file $GDB_LOG\" \
           -ex \"set debuginfod enabled on\" \
           -ex \"set logging enabled on\" \
@@ -94,7 +94,6 @@ if [[ $DEBUG_MODE -eq 1 ]]; then
           -ex \"handle SIGFPE stop print pass\" \
           -ex \"handle SIGILL stop print pass\" \
           --args ./worldserver -c ../etc/worldserver.conf"
-
 else
 
     # via acore.sh for auto-restart
